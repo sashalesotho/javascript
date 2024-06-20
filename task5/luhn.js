d7a528d36fa3f0048c37ca74e60daca2b8294ff4
Sasha <na-okne@mail.ru>
2024-06-20 14:37:32 +0300

    task5

---
+++ task5/luhn.js
@@ -0,0 +1,19 @@
+//
+// This is only a SKELETON file for the 'Luhn' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const valid = (num) => {
+  const str = num.replace(/\s/g, '');
+  if (str.length <= 1) return false;
+  if (str.match(/\D/g)) return false;
+  const arr1 = [];
+  for (let i = str.length - 2; i >= 0; i -= 2) {
+    arr1.unshift(str[i] * 2 <= 9 ? str[i] * 2 : str[i] * 2 - 9);
+  }
+  const arr2 = [];
+  for (let i = str.length - 1; i >= 0; i -= 2) {
+    arr2.unshift(+str[i]);
+  }
+  return [...arr1, ...arr2].reduce((acc, el) => acc + el, 0) % 10 === 0;
+};

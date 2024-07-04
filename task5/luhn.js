d5db0f48a7b328a34bcaa4adc235f9157fe68099
Sasha <na-okne@mail.ru>
2024-07-04 04:41:59 +0300

    task5 fix

--- task5/luhn.js
+++ task5/luhn.js
@@ -4,18 +4,22 @@
 //
 
 export const valid = (num) => {
-  const str = num.replaceAll(' ', '');
-  if (str.length <= 1) return false;
-  for (let i = 0; i < num.length; i++) {
-    if (typeof (+num[i]) !== 'number') return false;
-  }
-  const arr1 = [];
-  for (let i = str.length - 2; i >= 0; i -= 2) {
-    arr1.unshift(str[i] * 2 <= 9 ? str[i] * 2 : str[i] * 2 - 9);
-  }
-  const arr2 = [];
-  for (let i = str.length - 1; i >= 0; i -= 2) {
-    arr2.unshift(+str[i]);
-  }
-  return [...arr1, ...arr2].reduce((acc, el) => acc + el, 0) % 10 === 0;
+	const str = num.replaceAll(" ", "");
+	if (str.length <= 1)
+		return false;
+	for (let i = 0; i < num.length; i++) {
+		if (typeof Number(num[i]) !== "number") return false;
+	}
+	const arr1 = [];
+	for (let i = str.length - 2; i >= 0; i -= 2) {
+		if (str[i] * 2 <= 9) 
+			{ arr1.unshift(str[i] * 2) }
+		else 
+			{ arr1.unshift(str[i] * 2 - 9) }
+	}
+	const arr2 = [];
+	for (let i = str.length - 1; i >= 0; i -= 2) {
+		arr2.unshift(Number(str[i]));
+	}
+	return [...arr1, ...arr2].reduce((acc, el) => acc + el, 0) % 10 === 0;
 };
